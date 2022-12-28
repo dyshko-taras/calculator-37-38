d2da9ec44bc64b9d6a900e51c905d2fd8d2f85b4
taras <dyshkotaras@gmail.com>
2022-12-29 00:33:12 +0200

    lessons 38

--- app/src/main/java/com/dyshkotaras/calculator/MainActivity.java
+++ app/src/main/java/com/dyshkotaras/calculator/MainActivity.java
@@ -3,6 +3,10 @@ package com.dyshkotaras.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editTextAnswer = findViewById(R.id.editTextAnswer);
+        Button buttonAnswer = findViewById(R.id.buttonAnswer);
+        TextView textViewIncorrectAnswer = findViewById(R.id.textViewIncorrectAnswer);
+        TextView textViewCorrectAnswer = findViewById(R.id.textViewCorrectAnswer);
+
+        buttonAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = editTextAnswer.getText().toString();
+                if (text.equals("15")) {
+                    textViewCorrectAnswer.setVisibility(View.VISIBLE);
+                    textViewIncorrectAnswer.setVisibility(View.INVISIBLE);
+                } else {
+                    textViewIncorrectAnswer.setVisibility(View.VISIBLE);
+                    textViewCorrectAnswer.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 }
